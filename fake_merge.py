656e63e3afbc37776008fc8c9642e9c05d0944d6
Prakhar <prakhardevsingh4@gmail.com>
2020-07-08 13:25:48 +0530

    Add files via upload

---
+++ fake_merge.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+l=os.listdir()
+
+data=pd.read_csv(l[0],sep='|',error_bad_lines=False)
+
+for i in range(1,len(l)):
+	if(l[i]!="main.csv"):
+		temp=pd.read_csv(l[i],sep='|',error_bad_lines=False)
+		data=pd.concat([data,temp])
+		data=data.reset_index(drop=True)
+
+
+data.to_csv("fake_meta.csv")
+
+
+
